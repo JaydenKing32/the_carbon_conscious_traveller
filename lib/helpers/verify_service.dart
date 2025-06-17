d9fe26465af6472b0602cc3e2a7006e75e785f97
JaydenKing32 <jaydenking32@gmail.com>
2025-06-18 00:29:17 +1000

    Moved dynamo_helper

--- lib/helpers/verify_service.dart
+++ lib/helpers/verify_service.dart
@@ -4,8 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_background_service/flutter_background_service.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:the_carbon_conscious_traveller/db/dynamo_helper.dart';
 import 'package:the_carbon_conscious_traveller/db/trip_database.dart';
+import 'package:the_carbon_conscious_traveller/helpers/dynamo_helper.dart';
 import 'package:the_carbon_conscious_traveller/models/trip.dart';
 
 @pragma("vm:entry-point")
