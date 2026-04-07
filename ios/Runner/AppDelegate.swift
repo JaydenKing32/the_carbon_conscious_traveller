9550f7f3b88ea9be820c4e63c8edf7dc89727a29
JaydenKing32 <jaydenking32@gmail.com>
2026-04-07 21:55:42 +1000

    iOS now uses UISceneDelegate

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,13 +3,16 @@ import UIKit
 import GoogleMaps
 
 @main
-@objc class AppDelegate: FlutterAppDelegate {
+@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
 
+  func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
+    GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
     // Read API key from Config.plist
     if let path = Bundle.main.path(forResource: "Config", ofType: "plist") {
       if let config = NSDictionary(contentsOfFile: path),
@@ -21,7 +24,5 @@ import GoogleMaps
     } else {
       fatalError("Config.plist file not found")
     }
-
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
